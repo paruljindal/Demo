a8995a1ad51be3e163debecb0da92cd4c37cfb57
Parul Jindal <jindalparul1996@gmail.com>
2019-11-14 22:31:14 +0530

    First Commit

---
+++ Desktop/practice/basic_socket.py
@@ -0,0 +1,13 @@
+import socket
+
+def get_hostname_ip():
+    try:
+        hostname=socket.gethostname()
+        hostip=socket.gethostbyname(hostname)
+        print("Hostname :",hostname)
+        print("IP :",hostip)
+    except:
+        print('unable to get Hostname and IP')
+
+get_hostname_ip()
+              
